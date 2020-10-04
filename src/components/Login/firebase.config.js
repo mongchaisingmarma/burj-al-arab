c1f438f30db8b081787e0ed019c37f010e925b9a
mongchaisingmarma <m.marma41413mongchaising@yahoo.com>
2020-10-04 12:12:29 +0600

    burj-al-arab complete

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAEVe6oLYJ1Nv_kMRCKjcVORqI8dOWa4zE",
+    authDomain: "burj-al-arab-2c809.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-2c809.firebaseio.com",
+    projectId: "burj-al-arab-2c809",
+    storageBucket: "burj-al-arab-2c809.appspot.com",
+    messagingSenderId: "514595493824",
+    appId: "1:514595493824:web:13d2fcaa6a593548bc1a25"
+  };
+
+  export default firebaseConfig;
